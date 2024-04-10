f84341a867eb96b9a36274e1dae5d920f1501da4
Gonzalo Diaz <egonzalodm@gmail.com>
2024-04-10 09:36:20 -0500

    cambios balon

--- PythonFiles/SoccerAgent1.py
+++ PythonFiles/SoccerAgent1.py
@@ -23,7 +23,7 @@ act_config = {
 
 ppo_config = PPOConfig()
 
-ppo_config = ppo_config.training(gamma=0.9, lr=0.1, entropy_coeff=0.01)
+ppo_config = ppo_config.training(gamma=0.9, lr=0.1, entropy_coeff=0.01, kl_coeff = 0.0)
 ppo_config = ppo_config.resources(num_gpus=0)
 ppo_config = ppo_config.rollouts(num_rollout_workers=0)
 
@@ -40,7 +40,7 @@ env_config = {
 unray_config = UnrayConfig()
 
 # Path
-path = "C:/Users/semil/Documents/modelos/Soccer"
+path = "E:/Universidad/Codigo/Nullspace/UE5/AgentGardenProject/Models/soccer-v2"
 
 # Create instance of single agent environment
 env = SingleAgentEnv(env_config, "car_env")
@@ -48,7 +48,7 @@ env = SingleAgentEnv(env_config, "car_env")
 # Create algo instance
 algo = unray_config.configure_algo(ppo_config, env)
 
-algo.restore(path) #= Algorithm.from_checkpoint(path)
+#algo.restore(path) #= Algorithm.from_checkpoint(path)
 mean_ = []
 min_ = []
 max_ = []
@@ -61,11 +61,12 @@ for i in range (200):
     min_.append(result['episode_reward_min'])
     max_.append(result['episode_reward_max'])
     episodes.append(result['episodes_total'])
-
+    print(result)
 
     if i % 5 == 0:
         save_result = algo.save(path)#("C:/Users/gonza/AppData/Local/Temp/tmp10hjh2wd")
         print("An Algorithm checkpoint has been created inside directory: "f"'{save_result}'.")
+        '''
         iters = [i for i in range(200)]
         plt.plot(iters, mean_, color='black', label='mean')
         plt.plot(iters, min_, ls='dashed', color='red', label='min')
@@ -74,7 +75,8 @@ for i in range (200):
         plt.title('PPO 15 Iteration training')
         plt.legend()
         plt.ylabel('reward')
-        plt.savefig('C:/Users/semil/Documents/modelos/Soccer/imagen.png')
+        plt.savefig(""f"'{path}'/imagen.png")
+        '''
 
 
 
