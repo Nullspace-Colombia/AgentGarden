47b06c04584410b7dbd9fcea112055959042d584
Valentina Hernández <vahernandezmo@unal.edu.co>
2024-06-05 10:37:00 -0500

    [TESTING] Testing different rewards on soccer env

--- PythonFiles/SoccerAgent1.py
+++ PythonFiles/SoccerAgent1.py
@@ -7,7 +7,7 @@ from ray.rllib.algorithms.algorithm import Algorithm
 import matplotlib.pyplot as plt
 
 high = np.array(
-    [10000]*30,
+    [10000]*36,
     dtype=np.float32,
 )
 
@@ -17,14 +17,14 @@ obs_config = {
 }
 
 act_config = {
-    "space": BridgeSpaces.MultiDiscrete([3]),
+    "space": BridgeSpaces.Discrete(5),
     "description": "Acciones"
 }
 
 ppo_config = PPOConfig()
 
-#ppo_config = ppo_config.training(gamma=0.9, lr=0.1, entropy_coeff=0.01, kl_coeff = 0.0)
-ppo_config = ppo_config.resources(num_gpus=0)
+ppo_config = ppo_config.training(gamma=0.99, lr=0.0001, clip_param=0.2, lambda_=0.95)
+ppo_config = ppo_config.resources(num_gpus=1)
 ppo_config = ppo_config.rollouts(num_rollout_workers=0)
 
 
@@ -40,10 +40,10 @@ env_config = {
 unray_config = UnrayConfig()
 
 # Path
-path = "C:/Users/semil/Documents/modelos/Soccer/V2" #"E:/Universidad/Codigo/Nullspace/UE5/AgentGardenProject/Models/soccer-v2"
+path = "C:/Users/Valentina/Documents/1_Universidad/AI/4_Tests/Soccer_Agent/R5" #"E:/Universidad/Codigo/Nullspace/UE5/AgentGardenProject/Models/soccer-v2"
 
 # Create instance of single agent environment
-env = SingleAgentEnv(env_config, "car_env")
+env = SingleAgentEnv(env_config, "soccer")
 
 # Create algo instance
 algo = unray_config.configure_algo(ppo_config, env)
@@ -54,21 +54,22 @@ min_ = []
 max_ = []
 episodes = []
 # Train
-for i in range (31):
+for i in range (51):
+    print("Iteración:"f" '{i}'")
     result = algo.train()
-    print("Episodio:"f" '{i}'")
-    mean_.append(result['episode_reward_mean'])
-    min_.append(result['episode_reward_min'])
-    max_.append(result['episode_reward_max'])
-    episodes.append(result['episodes_total'])
-    print(result)
-    print(len(min_))
+    #mean_.append(result['episode_reward_mean'])
+    #min_.append(result['episode_reward_min'])
+    #max_.append(result['episode_reward_max'])
+    #episodes.append(result['episodes_total'])
+    #print(result)
+    #print(len(min_))
+    print(f"EPISODE REWARD MEAN | {result['episode_reward_mean']} |")
 
     if i % 5 == 0:
 
         save_result = algo.save(path)#("C:/Users/gonza/AppData/Local/Temp/tmp10hjh2wd")
         print("An Algorithm checkpoint has been created inside directory: "f"'{save_result}'.")
-
+        """
         iters = [j for j in range(i+1)]
         plt.figure()
         plt.plot(iters, mean_, color='black', label='mean')
@@ -79,7 +80,7 @@ for i in range (31):
         plt.legend()
         plt.ylabel('reward')
         plt.savefig(f"Imagenes/iter_{i}")
-
+        """
 
 
 
