e2172aaefd19ff1a6c8585a793a41614c58ee171
Valentina Hernández <vahernandezmo@unal.edu.co>
2025-04-26 01:00:22 -0500

    [UPDATE] Update soccer env

---
+++ Plugins/unray/Source/unray/Classes/Inferencer.h
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include <random>
+
+#include "NNE.h"
+#include "NNERuntimeCPU.h"
+#include "NNEModelData.h"
+
+#include "Inferencer.generated.h"
+
+
+UENUM(BlueprintType)
+enum ActionSpaceType {
+	Discrete      UMETA(DisplayName = "Discrete"),
+	MultiDiscrete       UMETA(DisplayName = "MultiDiscrete"),
+	Box        UMETA(DisplayName = "Box"),
+	MultiBinary      UMETA(DisplayName = "MultiBinary"),
+};
+
+UCLASS()
+class UNRAY_API AInferencer : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AInferencer();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, Category = "Inference")
+	TObjectPtr<UNNEModelData> ModelData;
+
+	UFUNCTION(BlueprintCallable, Category = "Inference")
+	TArray<float>  makeInference(TArray<double> observations);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inference")
+	TEnumAsByte < ActionSpaceType > actionSpaceType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inference")
+	FString Shape;
+
+	void configureActionSpace();
+
+	TArray<float> getActions(TArray<float> OutputD);
+
+	TArray<float> Actions;
+
+	TConstArrayView<UE::NNE::FTensorDesc> OutputTensorDescs;
+	TArray<UE::NNE::FTensorShape> OutputTensorShapes;
+	TUniquePtr<UE::NNE::IModelInstanceCPU> ModelInstance;
+	TArray<float> OutputData;
+	TArray<UE::NNE::FTensorBindingCPU> InputBindings;
+	TArray<UE::NNE::FTensorBindingCPU> OutputBindings;
+	TArray<FString> Out;
+	TArray<float> nums;
+
+};
